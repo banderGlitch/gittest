e4dbc91bbe6ec9447f4aa80fe404d2640ed28cbb
Nernay Kumar <nernaykumar98@gmail.com>
2026-01-02 12:50:48 +0530

    testchanges

---
+++ index.js
@@ -0,0 +1,50 @@
+// github
+
+// Git = version control tool
+
+// Tracks code changes on your laptop
+
+
+// What is GitHub?
+
+// GitHub = online place to store Git repositories
+
+
+// Each GitHub account = one identity
+
+
+// Each GitHub account = one identity
+
+// SSH basics (this is the key part)
+
+//  Lets GitHub know who you are
+
+
+
+// ssh personal account public keys
+
+// ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3UdVfMe5/CG4dLqjHWwpvHhlY1H3rcnMmeJrDQQ87B nernaykumar98@gmail.com
+
+
+// ssh work account public keys
+
+// ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE8sXA6YS/B2t04XqzG+DcZpmmqsAPI8ubigH0f2TY+O nernaykumar@gmail.com
+
+
+
+//  we'll get something like this in the terminal
+// origin  git@github.com-personal:your-username/repo-name.git (fetch)
+
+
+//----------------------------------------------------------------
+
+
+// github.com-personal → personal SSH key
+// github.com-work     → work SSH key
+
+
+
+
+
+//----- github.com-personal → personal SSH key → personal GitHub account
+// github.com-work     → work SSH key → work GitHub account
